049e601af230fb8c68f6d900eb65f87085f1b7a8
ruby -v <yen.nnh794@gmail.com>
2017-07-16 21:26:28 +0700

    add display done or udone

--- week 1/esingment/todo/List.rb
+++ week 1/esingment/todo/List.rb
@@ -1,27 +1,53 @@
+require_relative 'item'
 class List
 attr_accessor :items
 attr_accessor :name
 
-  def initialize(name, items = [ ])
+  def initialize(name, items = [])
     @items = items
     @name = name
 
   end
 
-  def items
-     @items 
-  end 
   
-  def name
-    @name
+  def add(new_item)
+    @items << new_item
   end
-  
-  def add(items)
-    @items << items
-  def display
-    "display #{@name}"
-  end  
-end
-   b = List.new("Today's list", [List.new("first"), List.new("second")])
 
-puts b.items
+  def complete_at!(index)
+  @items[index].done = true
+  end
+
+  def display
+
+   puts "list : #{@name}"
+   @items.each do |item|
+      if items.done?
+       puts " -[x] #{item.name}"
+      else 
+        puts "[ ] #{item.name}"
+      end
+    end
+end  
+
+def display_done
+    puts "Done  :"
+    @items.each do |item|
+      if item.done?
+        puts "[x] #{item.name}"
+        end
+      end
+   end
+    
+ def display_undone   
+    puts " unDone :" 
+    @items.each do |item|
+      if item.done?
+        puts "[ ] #{item.name}"
+        end
+      end
+   end
+
+#      b = List.new("Today's list", [List.new("first"), List.new("second")])
+
+# puts b.List.new
